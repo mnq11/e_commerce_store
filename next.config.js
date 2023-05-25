909b4b8c39e98bd82eca93bc588135f787b09f68
mnq11 <mnq_77@yahoo.com>
2023-05-25 06:53:06 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true
+  },
+  images: {
+    domains: ['cdn.schema.io']
+  }
+}
 
 module.exports = nextConfig
